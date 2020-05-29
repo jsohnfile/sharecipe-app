dea81183b4d4784c3bd1735a8abafce6509a2291
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-05-29 14:51:42 -0600

    Add working oauth

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+}
+
 module.exports = router;
