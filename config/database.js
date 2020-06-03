78c66bfc4479d7e8385c36d29a8e8df824bd2118
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-06-02 20:41:50 -0600

    Connect to MongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/sharecipes',{
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-});
+  });
 
 const db= mongoose.connection;
 db.on('connected', function(){
     console.log(`Connected to MongoDB at ${db.host}: ${db.port}`)
 });
+
